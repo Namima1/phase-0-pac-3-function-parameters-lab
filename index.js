44a99ac00fcab9138534eb5e9f8cd830bf187c44
Namima Islam <namimaislam308@gmail.com>
2024-05-13 17:28:12 -0400

    adds myfile.md

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function introduction(name){
-    return('Hi, my name is ${name}.');
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
 }
+
+console.log(introduction("Aki"));
+
+
 
 function introductionWithLanguage(name, language) {
-    return('Hi, my name is [name] and I am learning to program in [language].');
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional(name, language = "Javascript") {
-    return('Hi, my name is [name] and I am learning to program in [language].');
+console.log(introductionWithLanguage("Aki", "Ember.js"));
+
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
+
+console.log(introductionWithLanguageOptional("Gracie"));
